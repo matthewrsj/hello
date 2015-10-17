a279c5bef22627106baa20175d20e4760be0abd5
VJohnson1202 <mrs.johnson_1202@yahoo.com>
2015-10-16 19:37:47 -0700

    Create hello-world-cl.cpp

---
+++ hello-world-cl.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "Hello world!" << endl;      // English
+    cout << "Hola mundo!" << endl;       // Spanish
+    cout << "Bonjour le monde!" << endl; // French
+    cout << "Hallo verden!" << endl;     // Norwegian
+        
+    return 0;
+}
