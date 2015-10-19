ced4fa5fe21511d0a06bd03bb227b85b94685a27
VJohnson1202 <mrs.johnson_1202@yahoo.com>
2015-10-19 10:49:04 -0700

    Create hello-world-cl.c

---
+++ hello-world-cl.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() 
+{
+  printf("Hello world!");          /* English    */
+  printf("Hola mundo!");           /* Spanish    */
+  printf("Bonjour le monde!");     /* French     */
+  printf("Hallo verden!");         /* Norwegian  */
+}
