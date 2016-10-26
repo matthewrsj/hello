14291f4e06e0ec00427a16792c88a51efe62c667
VJohnson1202 <mrs.johnson_1202@yahoo.com>
2016-10-26 15:28:47 -0700

    Update hello-world-cl.java

--- hello-world-cl.java
+++ hello-world-cl.java
@@ -1,6 +1,9 @@
 public class HelloWorld {
-     public status void main(String[] args {
-          \\ Prints "Hello, World" in the terminal window.
+     public status void main(String[] args) {
+          \\ Prints "Hello world!" in the terminal window.
+          \\ Prints "Hola mundo!" in the terminal window.
+          \\ Prints "Bonjour le monde!" in the terminal window.
+          \\ Prints "Hallo verden!" in the terminal window.
           System.out.println("Hello, World");
      }
 }
